6638c8ccda61cbbab895c208a97cf14a01283d95
Monsuru Okuniyi <okuniyimonsuru@yahoo.com>
2023-12-12 09:47:34 +0100

    feat: project setup
    - initial commit with project setup

---
+++ src/test/java/com/mon/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mon.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
