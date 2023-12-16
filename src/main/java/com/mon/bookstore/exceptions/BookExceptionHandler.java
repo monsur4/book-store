3906dd3186d058bbfd59f36c14f08f00a3486bc6
Monsuru Okuniyi <okuniyimonsuru@yahoo.com>
2023-12-16 16:29:12 +0100

    fix: invalid request payload
    - add MethodArgumentNotValidException, ConstraintViolationException and GenericException handlers
    
    - add HttpErrorItem to list out invalid fields

--- src/main/java/com/mon/bookstore/exceptions/BookExceptionHandler.java
+++ src/main/java/com/mon/bookstore/exceptions/BookExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.mon.bookstore.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.ConstraintViolationException;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -7,8 +9,12 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.*;
 
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -16,8 +22,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class BookExceptionHandler {
 
     @ExceptionHandler(BookNotFoundException.class)
-    public ResponseEntity<Object> handleBookNotFoundException(BookNotFoundException ex){
-        log.debug("handling BookNotFoundException");
+    public ResponseEntity<Object> handleBookNotFoundException(BookNotFoundException ex) {
+        log.warn("handling BookNotFoundException");
         HttpStatus status = HttpStatus.NOT_FOUND;
         HttpError httpError = HttpError.builder().message(ExceptionHelper.getRootCause(ex).getMessage())
                 .status(status)
@@ -27,9 +33,54 @@ public class BookExceptionHandler {
     }
 
     @ExceptionHandler(BookServiceException.class)
-    public ResponseEntity<Object> handleBookServiceException(BookServiceException ex){
-        log.debug("handling BookServiceException");
+    public ResponseEntity<Object> handleBookServiceException(BookServiceException ex) {
+        log.warn("handling BookServiceException : {}", ex.getMessage(), ex);
         HttpStatus status = HttpStatus.BAD_REQUEST;
+        HttpError httpError = HttpError.builder().message(ExceptionHelper.getRootCause(ex).getMessage())
+                .status(status)
+                .build();
+
+        return new ResponseEntity<>(httpError, status);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
+        log.warn("handling ConstraintViolationException : {}", ex.getMessage(), ex);
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        HttpError httpError = HttpError.builder().message(ExceptionHelper.getRootCause(ex).getMessage())
+                .status(status)
+                .build();
+
+        return new ResponseEntity<>(httpError, status);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.warn("handling MethodArgumentNotValidException: {}", ex.getMessage(), ex);
+
+        Map<String, Object> jsonErrors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(err -> {
+            String field = ((FieldError) err).getField();
+            String defaultMessage = err.getDefaultMessage();
+            Object rejectedValue = ((FieldError) err).getRejectedValue();
+
+            jsonErrors.put(field, defaultMessage + " => [rejected value: " + rejectedValue + "]");
+        });
+
+        List<HttpErrorItem> httpErrorItems = HttpErrorItem.fromMap(jsonErrors);
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        HttpError httpError = HttpError.builder().message("Invalid request payload")
+                .status(status)
+                .errors(httpErrorItems)
+                .build();
+
+        return new ResponseEntity<>(httpError, status);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGenericException(Exception ex) {
+        log.warn("handling GenericException: {}", ex.getMessage(), ex);
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         HttpError httpError = HttpError.builder().message(ExceptionHelper.getRootCause(ex).getMessage())
                 .status(status)
                 .build();
@@ -39,8 +90,31 @@ public class BookExceptionHandler {
 
     @Data
     @Builder
-    public static class HttpError{
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class HttpError {
         private String message;
         private HttpStatus status;
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        private List<HttpErrorItem> errors;
+    }
+
+    @Data
+    @Builder
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class HttpErrorItem {
+        private String message;
+
+        private String field;
+
+        public static List<HttpErrorItem> fromMap(Map<String, Object> errorMap) {
+            List<HttpErrorItem> errorList = new ArrayList<>();
+            errorMap.forEach((field, message) -> errorList.add(
+                    HttpErrorItem.builder()
+                            .message((String) message)
+                            .field(field)
+                            .build()
+            ));
+            return errorList;
+        }
     }
 }
